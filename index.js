e0335be32382f6ebd5e5f5f000b3788f922fb918
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-01 20:39:04 +0200

    latest

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ import cors from "cors";
 
 const PORT = 3001;
 
-const getToken = () => {
-  const client = stream.connect(
-    process.env.API_KEY,
-    process.env.API_SECRET,
-    process.env.API_ID
-  );
+const client = stream.connect(
+  process.env.API_KEY,
+  process.env.API_SECRET,
+  process.env.API_ID
+);
 
-  const userToken = client.createUserToken("user1");
-
-  return userToken;
-};
+const userToken = client.createUserToken("user1");
 
 const app = express();
 
 app.use(cors());
 
 app.get("/token", (req, res) => {
-  const token = getToken();
+  res.json({ userToken });
+});
+
+app.get("/addUser", () => {
   user1 = client.feed("user", "1");
   activity = { actor: 1, verb: "tweet", object: 1, foreign_id: "tweet:1" };
   user1.addActivity(activity);
-  res.json({ token });
+
+  res.send("succes");
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
