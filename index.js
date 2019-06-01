04389561b18a10cdb701061d64c10fd230b296ee
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-01 19:54:59 +0200

    srver side config

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-var stream = require("getstream");
-// Instantiate a new client (server side)
+import stream from "getstream";
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+console.log(process.env.API_KEY, process.env.API_SECRET, process.env.API_ID);
 
 const getToken = async () => {
-  const client = stream.connect(
+  const client = await stream.connect(
     process.env.API_KEY,
     process.env.API_SECRET,
     process.env.API_ID
   );
-  let userToken = await client.createUserToken("user-one");
+
+  const userToken = client.createUserToken("user-one");
+
+  return userToken;
 };
 
 console.log("this isi the user token", getToken());
+
+const app = express();
+
+app.use(cors());
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
