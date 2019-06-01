8b26699ce321d4cfb9d9ffa402ae915fea8827ec
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-01 20:54:44 +0200

    fixing feed

--- index.js
+++ index.js
@@ -13,31 +13,31 @@ const client = stream.connect(
   process.env.API_ID
 );
 
-const token = client.createUserToken("user:1");
-const user1 = client.feed("user");
+const token = client.createUserToken("user", "1");
+const user1 = client.feed("user", "1");
 let activity = { actor: 1, verb: "tweet", object: 1, foreign_id: "tweet:1" };
 
 app.get("/token", (req, res) => {
   res.json({ token });
 });
 
-app.get("/addActivity1", (req, res) => {
+app.get("/addActivity", (req, res) => {
   user1.addActivity(activity);
   res.send("succes");
 });
 
-app.get("/addActivity2", (req, res) => {
-  activity = {
-    actor: 1,
-    verb: "run",
-    object: 1,
-    foreign_id: "run:1",
-    course: { name: "Golden Gate park", distance: 10 },
-    participants: ["Thierry", "Tommaso"],
-    started_at: new Date()
-  };
-  user1.addActivity(activity);
-  res.send("succes");
-});
+// app.get("/addActivity2", (req, res) => {
+//   activity = {
+//     actor: 1,
+//     verb: "run",
+//     object: 1,
+//     foreign_id: "run:1",
+//     course: { name: "Golden Gate park", distance: 10 },
+//     participants: ["Thierry", "Tommaso"],
+//     started_at: new Date()
+//   };
+//   user1.addActivity(activity);
+//   res.send("succes");
+// });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
