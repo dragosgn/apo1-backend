41b1f0b324e5e4645186029687ee2f903baaf564
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-01 20:50:56 +0200

    latest

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express from "express";
 import cors from "cors";
 
 const PORT = 3001;
+const app = express();
+app.use(cors());
 
 const client = stream.connect(
   process.env.API_KEY,
@@ -11,21 +13,30 @@ const client = stream.connect(
   process.env.API_ID
 );
 
-const userToken = client.createUserToken("user1");
-
-const app = express();
-
-app.use(cors());
+const token = client.createUserToken("user:1");
+const user1 = client.feed("user");
+let activity = { actor: 1, verb: "tweet", object: 1, foreign_id: "tweet:1" };
 
 app.get("/token", (req, res) => {
-  res.json({ userToken });
+  res.json({ token });
 });
 
-app.get("/addUser", () => {
-  user1 = client.feed("user", "1");
-  activity = { actor: 1, verb: "tweet", object: 1, foreign_id: "tweet:1" };
+app.get("/addActivity1", (req, res) => {
   user1.addActivity(activity);
+  res.send("succes");
+});
 
+app.get("/addActivity2", (req, res) => {
+  activity = {
+    actor: 1,
+    verb: "run",
+    object: 1,
+    foreign_id: "run:1",
+    course: { name: "Golden Gate park", distance: 10 },
+    participants: ["Thierry", "Tommaso"],
+    started_at: new Date()
+  };
+  user1.addActivity(activity);
   res.send("succes");
 });
 
